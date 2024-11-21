bc4d3267b97c6ae98a058f07bbd6db80773cd997
adam.walid <adam.0522018@gmaqil.com>
2024-11-21 10:32:21 +0200

    pull request commit

--- Adam_Waleed_0522028_api/Repo/MoviesRepo/IMovies_Repo.cs
+++ Adam_Waleed_0522028_api/Repo/MoviesRepo/IMovies_Repo.cs
@@ -6,6 +6,7 @@ namespace Adam_Waleed_0522028_api.Repo.MoviesRepo
     {
            public IList<Movies_Dto> GetAll();
            public Movies_Dto_getById GetById(int id);
+
            public void AddMovies(Moives_Dto_Add dto);
     }
 }
